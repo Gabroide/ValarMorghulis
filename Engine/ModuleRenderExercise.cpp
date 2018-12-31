f8508866a182ad70dbf67219c43b9fb1c4f04287
_gaspode <gaspode@uoc.edu>
2018-12-31 17:05:17 +0100

    Minor changes performed

--- Engine/ModuleRenderExercise.cpp
+++ Engine/ModuleRenderExercise.cpp
@@ -73,7 +73,8 @@ update_status ModuleRenderExercise::Update()
 	math::float4x4 Model(math::float4x4::identity); // Not moving anything
 
 	glUniformMatrix4fv(glGetUniformLocation(program, "proj"), 1, GL_TRUE, &App->camera->ProjectionMatrix()[0][0]);
-	glUniformMatrix4fv(glGetUniformLocation(program, "view"), 1, GL_TRUE, &App->camera->LookAt(App->camera->cameraPos, App->camera->cameraPos + App->camera->cameraFront, App->camera->cameraUp)[0][0]);
+	// We are using the vector front pointing to our target instead of target - cameraPos so we can manage easily with vectors
+	glUniformMatrix4fv(glGetUniformLocation(program, "view"), 1, GL_TRUE, &App->camera->LookAt(App->camera->cameraPos, App->camera->cameraFront, App->camera->cameraUp)[0][0]);
 	glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_TRUE, &Model[0][0]);
 
 	// Draw every GL_TRIANGLE that starts at vec[0] and you can find 3 of them
@@ -101,7 +102,6 @@ void ModuleRenderExercise::DrawReferenceGround()
 	glLineWidth(1.0f);
 	float d = 200.0f;
 	glBegin(GL_LINES);
-
 	for (float i = -d; i <= d; i += 1.0f)
 	{
 		glVertex3f(i, 0.0f, -d);
@@ -109,7 +109,6 @@ void ModuleRenderExercise::DrawReferenceGround()
 		glVertex3f(-d, 0.0f, i);
 		glVertex3f(d, 0.0f, i);
 	}
-
 	glEnd();
 }
 
