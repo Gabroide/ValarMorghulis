977f01929c64523ae78eaaf46fc2141157a0d702
_gaspode <gaspode@uoc.edu>
2019-01-03 20:43:27 +0100

    Performed the skeleton of  Mesh Component

---
+++ Engine/ComponentMesh.cpp
@@ -0,0 +1,11 @@
+#include "ComponentMesh.h"
+
+// Constructor
+ComponentMesh::ComponentMesh()
+{
+}
+
+// Destructor
+ComponentMesh::~ComponentMesh()
+{
+}
---
+++ Engine/ComponentMesh.h
@@ -0,0 +1,13 @@
+#ifndef __ComponentMesh_h__
+#define __ComponentMesh_h__
+
+#include "Component.h"
+
+class ComponentMesh : public Component
+{
+public:
+	ComponentMesh();
+	~ComponentMesh();
+};
+
+#endif // __Component_h__
