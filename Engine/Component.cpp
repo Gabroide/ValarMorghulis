8ed70e662a521da37d65f2c662778cd63a0cd564
_gaspode <gaspode@uoc.edu>
2019-01-03 20:33:48 +0100

    Component class performed

---
+++ Engine/Component.cpp
@@ -0,0 +1,28 @@
+#include "Component.h"
+
+// Constructor
+Component::Component()
+{
+
+}
+
+// Destructor
+Component::~Component()
+{
+
+}
+
+void Component::Enable()
+{
+	
+}
+
+void Component::Update()
+{
+	
+}
+
+void Component::Disable()
+{
+	
+}
---
+++ Engine/Component.h
@@ -0,0 +1,27 @@
+#ifndef __Component_h__
+#define __Component_h__
+
+#include "GameObject.h"
+
+class GameObject;
+
+enum class componentType
+{
+	CAMERA,
+	TRANSFORM,
+	MESH,
+	MATERIAL
+};
+
+class Component
+{
+public:
+	Component();
+	~Component();
+
+	virtual void Enable();
+	virtual void Update();
+	virtual void Disable();
+
+};
+
