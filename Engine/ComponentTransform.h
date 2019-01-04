7208be3de05b3308f337b20e0bc23d652cfcfbf8
_gaspode <gaspode@uoc.edu>
2019-01-04 23:52:07 +0100

    ComponentTransform has been implemented

--- Engine/ComponentTransform.h
+++ Engine/ComponentTransform.h
@@ -3,11 +3,25 @@
 
 #include "Component.h"
 
+#include "MathGeoLib.h"
+
+#include "IMGUI\imgui.h"
+
 class ComponentTransform : public Component
 {
 public:
-	ComponentTransform(GameObject* goContainer);
+	ComponentTransform(GameObject* goContainer, const aiMatrix4x4 &transform);
 	~ComponentTransform();
+
+	void			AddTransform(const aiMatrix4x4 &transform);
+
+public:
+	math::float3	position		= math::float3::zero;
+	math::float3	eulerRotation	= math::float3::zero;
+	math::float3	scale			= math::float3::zero;
+
+	math::Quat		rotation		= math::Quat::identity;
+
 };
 
 #endif // __ComponentTransform_h__
