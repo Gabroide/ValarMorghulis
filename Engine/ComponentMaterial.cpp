e26a46ab6627c5abcb55784877c0a1e6b4cd09dd
_gaspode <gaspode@uoc.edu>
2019-01-05 09:11:38 +0100

    ComplementMaterial implmented

--- Engine/ComponentMaterial.cpp
+++ Engine/ComponentMaterial.cpp
@@ -1,13 +1,45 @@
 #include "ComponentMaterial.h"
+#include "ModuleTextures.h"
+#include "Application.h"
 
 // Constructor
-ComponentMaterial::ComponentMaterial(GameObject* goContainer) : Component(goContainer, ComponentType::MATERIAL)
+ComponentMaterial::ComponentMaterial(GameObject* goContainer, const aiMaterial* material) : Component(goContainer, ComponentType::MATERIAL) 
 {
 
 }
 
 // Destructor
-ComponentMaterial::~ComponentMaterial()
+ComponentMaterial::~ComponentMaterial() 
 {
+	
+}
 
+void ComponentMaterial::SetMaterial(const aiMaterial* material) 
+{
+	std::string texturePath;
+	if (material != nullptr) 
+	{
+		aiTextureMapping mapping = aiTextureMapping_UV;
+		aiString file;
+		material->GetTexture(aiTextureType_DIFFUSE, 0, &file, &mapping, 0);
+		texturePath = goContainer->GetFileFolder() + file.C_Str();
+	}
+	else 
+	{
+		texturePath = "checkersTexture.jpg";
+	}
+
+	DeleteTexture();
+	texture = App->textures->Load(texturePath.c_str());
+}
+
+void ComponentMaterial::DeleteTexture() 
+{
+	if (texture != nullptr) 
+	{
+		glDeleteTextures(1, (GLuint*)&texture->id);
+	}
+
+	delete texture;
+	texture = nullptr;
 }
--- Engine/ComponentMaterial.h
+++ Engine/ComponentMaterial.h
@@ -1,13 +1,30 @@
 #ifndef __ComponentMaterial_h__
 #define __ComponentMaterial_h__
 
+#include "assert.h"
 #include "Component.h"
+
+#include "glew-2.1.0\include\GL\glew.h"
+
+#include "IMGUI\imgui.h"
+
+#include "assimp\material.h"
 
 class ComponentMaterial : public Component
 {
 public:
-	ComponentMaterial(GameObject* goContainer);
+	ComponentMaterial(GameObject* goContainer, const aiMaterial* material);
 	~ComponentMaterial();
+
+	void			SetMaterial(const aiMaterial* material);
+	void			DeleteTexture();
+
+public:
+	unsigned int	shader = 0u;
+
+public:
+	Texture*		texture = nullptr;
+
 };
 
 #endif // __ComponentMaterial_h__
