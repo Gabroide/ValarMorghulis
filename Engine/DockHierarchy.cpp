e880e7d3e0e2551b093867f72f05251bf48df95c
_gaspode <gaspode@uoc.edu>
2019-01-08 14:51:44 +0100

    Now it shows the new Game Object even if its is empty

--- Engine/DockHierarchy.cpp
+++ Engine/DockHierarchy.cpp
@@ -1,12 +1,15 @@
 #include "DockHierarchy.h"
 #include "Application.h"
 #include "ModuleScene.h"
+#include "ModuleInput.h"
 #include "GameObject.h"
+
+#include "SDL\include\SDL_mouse.h"
 
 // Constructor
 DockHierarchy::DockHierarchy() 
 {
-	
+
 }
 
 // Destructor
@@ -17,9 +20,27 @@ DockHierarchy::~DockHierarchy()
 
 void DockHierarchy::Draw() 
 {
-	if (!ImGui::Begin("Hierarchy", &enabled)) {
+	if (!ImGui::Begin("Hierarchy", &enabled)) 
+	{
 		ImGui::End();
+	
 		return;
+	}
+
+	// If empty heriarchy, allow to create one with a imgui popup
+	if (App->input->GetMouseButtonDown(SDL_BUTTON_RIGHT) == KEY_DOWN && ImGui::IsWindowHovered()) 
+	{
+		ImGui::OpenPopup("Modify_GameObject");
+	}
+
+	if (ImGui::BeginPopup("Modify_GameObject")) 
+	{
+		if (ImGui::Selectable("Add Empty GameObject")) 
+		{
+			App->scene->CreateGameObject();
+		}
+
+		ImGui::EndPopup();
 	}
 
 	App->scene->DrawHierarchy();
